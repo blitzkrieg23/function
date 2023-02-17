5ecc1c17cb705a961ba0238bd6f32e2d0ae0bebc
xzqt23 <angelo.agualada@gmail.com>
2023-02-17 11:17:09 +0800

    Create function.py

---
+++ function.py
@@ -0,0 +1,23 @@
+"""
+A parameter is the variable listed inside the parentheses in the function definition.
+
+An argument is the value that is sent to the function when it is called.
+"""
+
+def func():
+    print("Hello World")
+
+
+func()
+
+
+"""
+*** Add two numbers
+
+def add(num1,num2):
+    print(num1+num2) 
+
+add(4,2)
+
+"""
+
